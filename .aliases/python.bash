fd6b76c830f9063e2fed2af916f6562398f00adf
Andrea Rogers <targetdisk1394@gmail.com>
2024-11-12 11:05:02 -0600

    aliases: Python keeps getting worse!

--- .aliases/python.bash
+++ .aliases/python.bash
@@ -2,3 +2,5 @@
 alias mk-venv='python -m venv .venv'
 alias do-venv='. .venv/bin/activate'
 alias rm-venv='rm -rf .venv'
+[ $(command -v pdb) ] || alias pdb='python -m pdb'
+[ $(command -v pip) ] || alias pip='python -m pip'
