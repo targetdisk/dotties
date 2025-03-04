85e22af1f6b05a3157a41050a330071b8ac95eff
Andrea Rogers <targetdisk1394@gmail.com>
2025-03-04 16:58:22 -0600

    aliases: Make Python checks less chatty

--- .aliases/python.bash
+++ .aliases/python.bash
@@ -2,5 +2,6 @@
 alias mk-venv='python -m venv .venv'
 alias do-venv='. .venv/bin/activate'
 alias rm-venv='rm -rf .venv'
-[ $(command -v pdb) ] || alias pdb='python -m pdb'
-[ $(command -v pip) ] || alias pip='python -m pip'
+
+command -v pdb > /dev/null || alias pdb='python -m pdb'
+command -v pip > /dev/null || alias pip='python -m pip'
