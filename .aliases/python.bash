8f1e5a6b6873ae394f135887a5a8b584200adfb5
Andrea Rogers <targetdisk1394@gmail.com>
2026-02-09 22:59:52 -0600

    aliases: Fix Python command checks

--- .aliases/python.bash
+++ .aliases/python.bash
@@ -3,5 +3,5 @@ alias mk-venv='python -m venv .venv'
 alias do-venv='. .venv/bin/activate'
 alias rm-venv='rm -rf .venv'
 
-command -v pdb > /dev/null || alias pdb='python -m pdb'
-command -v pip > /dev/null || alias pip='python -m pip'
+[ -n "$(command -v pdb)" ] || alias pdb='python -m pdb'
+[ -n "$(command -v pip)" ] || alias pip='python -m pip'
